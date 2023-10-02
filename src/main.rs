e6ce005607380aad4071009f0a164c758be07e19
tsorabel <tom.sorabella@gmail.com>
2023-10-02 14:21:03 +0200

    Work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-extern crate obj_parser;
+use crate::window::init_window;
 
+extern crate gl;
+extern crate glfw;
+extern crate obj_parser;
 mod init;
+mod shader;
 mod utils;
+mod window;
 
 fn main() {
     let file_path = init::parse_argv();
     let obj_content = obj_parser::file::read_file(&file_path);
     let data = obj_parser::parse::pars_obj(&obj_content, &file_path);
-    println!("{:#?}\n{:#?}\n", data.0.unwrap(), data.1);
+    println!("{:#?}\n{:#?}\n", data.0, data.1);
+    init_window();
 }
---
+++ src/window.rs
@@ -0,0 +1,116 @@
+use crate::glfw::{Action, Context, Key};
+use std::sync::mpsc::Receiver;
+
+use crate::gl::types::*;
+
+use crate::shader::Shader;
+use std::mem;
+use std::os::raw::c_void;
+use std::ptr;
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                // make sure the viewport matches the new window dimensions; note that width and
+                // height will be significantly larger than specified on retina displays.
+                unsafe { gl::Viewport(0, 0, width, height) }
+            }
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
+            }
+            _ => {}
+        }
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn init_window() {
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    let (mut window, _event) = glfw
+        .create_window(800, 600, "tsorabel scop", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    let (ourShader, VAO) = unsafe {
+        let ourShader = Shader::new("src/shaders/shader.vs", "src/shaders/shader.fs"); // you can name your shader files however you like)
+
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        // ------------------------------------------------------------------
+        // HINT: type annotation is crucial since default for float literals is f64
+        let vertices: [f32; 18] = [
+            // positions         // colors
+            0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // bottom right
+            -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom left
+            0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // top
+        ];
+        let (mut VBO, mut VAO) = (0, 0);
+        gl::GenVertexArrays(1, &mut VAO);
+        gl::GenBuffers(1, &mut VBO);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        gl::BindVertexArray(VAO);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            &vertices[0] as *const f32 as *const c_void,
+            gl::STATIC_DRAW,
+        );
+
+        let stride = 6 * mem::size_of::<GLfloat>() as GLsizei;
+        // position attribute
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
+        gl::EnableVertexAttribArray(0);
+        // color attribute
+        gl::VertexAttribPointer(
+            1,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            stride,
+            (3 * mem::size_of::<GLfloat>()) as *const c_void,
+        );
+        gl::EnableVertexAttribArray(1);
+
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        // gl::BindVertexArray(0);
+
+        (ourShader, VAO)
+    };
+
+    while !window.should_close() {
+        // events
+        // -----
+        process_events(&mut window, &_event);
+
+        // render
+        // ------
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // render the triangle
+            ourShader.useProgram();
+            gl::BindVertexArray(VAO);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+}
